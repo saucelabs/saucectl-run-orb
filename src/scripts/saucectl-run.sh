68b3411ec0b4b605909848d1ec534e66de292f03
Félix P <felix@saucelabs.com>
2021-02-02 12:32:27 -0800

    Update shell checks

--- src/scripts/saucectl-run.sh
+++ src/scripts/saucectl-run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 bash --version
-reldir=`dirname $0`
+reldir=$(dirname $0)
 
 echo "Rel dir: ${reldir}"
-echo "PWD: `pwd`"
+echo "PWD: $(pwd)"
 
 
-bash ${reldir}/install.sh
+bash "${reldir}/install.sh"
 
 echo RUN
