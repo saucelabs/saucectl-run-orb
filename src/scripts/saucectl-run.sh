b2ee361dae3ab92976620ca6704b710e71cd10f1
Félix P <felix@saucelabs.com>
2021-02-02 12:31:17 -0800

    Troubleshoot

--- src/scripts/saucectl-run.sh
+++ src/scripts/saucectl-run.sh
@@ -1,29 +1,12 @@
-# PARAM_SAUCECTL_VERSION: <<parameters.saucectl_version>>
-# PARAM_SAUCE_USERNAME: <<parameters.sauce_username>>
-# PARAM_SAUCE_ACCESS_KEY: <<parameters.sauce_access_key>>
-# PARAM_WORKING_DIRECTORY: <<parameters.working_directory>>
-# PARAM_CONFIG_FILE: <<parameters.config_file>>
-# PARAM_REGION: <<parameters.region>>
-# PARAM_TESTING_ENVIRONMENT: <<parameters.testing_environment>>
-# PARAM_SKIP_RUN: <<parameters.skip_run>>
-# PARAM_SUITE: <<parameters.suite>>
+#!/usr/bin/env bash
 
-# run() {
+bash --version
+reldir=`dirname $0`
 
-#     export SAUCE_USERNAME=${PARAM_SAUCE_USERNAME}
-#     export SAUCE_ACCESS_KEY=${PARAM_SAUCE_ACCESS_KEY}
+echo "Rel dir: ${reldir}"
+echo "PWD: `pwd`"
 
-#     ${SAUCECTL_BINARY} ${}
-#     echo Hello "${PARAM_TO}"
-# }
 
-# # Will not run if sourced for bats-core tests.
-# # View src/tests for more information.
-# ORB_TEST_ENV="bats-core"
-# if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
-#     run
-# fi
-
-bash ./install.sh
+bash ${reldir}/install.sh
 
 echo RUN
