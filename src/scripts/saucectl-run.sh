ec07b375c0194eefcc6c137d36c7bd7833786867
Félix P <felix@saucelabs.com>
2021-10-05 13:18:06 -0700

    Remove deprecated elements /  add new params

--- src/scripts/saucectl-run.sh
+++ src/scripts/saucectl-run.sh
@@ -65,12 +65,8 @@ parse_args() {
         ARGS+=("--region" "${PARAM_REGION}")
     fi
 
-    if [ -n "${PARAM_TESTING_ENVIRONMENT}" ];then
-        ARGS+=("--test-env" "${PARAM_TESTING_ENVIRONMENT}")
-    fi
-
     if [ -n "${PARAM_SUITE}" ];then
-        ARGS+=("--suite" "${PARAM_SUITE}")
+        ARGS+=("--select-suite" "${PARAM_SUITE}")
     fi
 
     if [ -n "${PARAM_WORKING_DIRECTORY}" ];then
@@ -94,20 +90,32 @@ parse_args() {
         done <<< "${PARAM_ENV}"
     fi
 
-    if [ -n "${PARAM_LOGDIR}" ];then
-        ARGS+=("--logDir" "${PARAM_LOGDIR}")
-    fi
-
     if [ -n "${PARAM_TIMEOUT}" ];then
         ARGS+=("--timeout" "${PARAM_TIMEOUT}")
     fi
 
-    if [ -n "${PARAM_TUNNEL_ID}" ];then
-        ARGS+=("--tunnel-id" "${PARAM_TUNNEL_ID}")
+    if [ -n "${PARAM_TUNNEL_NAME}" ];then
+        ARGS+=("--tunnel-name" "${PARAM_TUNNEL_NAME}")
     fi
 
-    if [ -n "${PARAM_TUNNEL_PARENT}" ];then
-        ARGS+=("--tunnel-parent" "${PARAM_TUNNEL_PARENT}")
+    if [ -n "${PARAM_TUNNEL_OWNER}" ];then
+        ARGS+=("--tunnel-owner" "${PARAM_TUNNEL_OWNER}")
+    fi
+
+    if [ -n "${PARAM_CCY}" ];then
+        ARGS+=("--ccy" "${PARAM_CCY}")
+    fi
+
+    if [ -n "${PARAM_RETRIES}" ];then
+        ARGS+=("--retries" "${PARAM_RETRIES}")
+    fi
+
+    if [ -n "${PARAM_RETRIES}" ];then
+        ARGS+=("--retries" "${PARAM_RETRIES}")
+    fi
+
+    if [ -n "${PARAM_TEST_ENV_SILENT}" ];then
+        ARGS+=("--test-env-silent")
     fi
 }
 
